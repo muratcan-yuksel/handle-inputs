1659575b4cda1cc6e9685ee5e0dcb419c3f3dca1
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-30 00:04:12 +0300

    trying to push the array with the user input

--- src/Overview.js
+++ src/Overview.js
@@ -6,8 +6,20 @@ class Overview extends Component {
 
     this.state = {
       items: ["take the dog out", "wash the dishes", "code"],
+      input: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleItems = this.handleItems.bind(this);
   }
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+  handleItems = (event) => {
+    this.setState({
+      items: this.state.items.push(this.state.value),
+    });
+    console.log(this.state.value);
+  };
 
   render() {
     let try2 = this.state.items;
@@ -16,8 +28,14 @@ class Overview extends Component {
     return (
       <div>
         <label>Tasks</label>
-        <input type="text"></input>
-        <button type="submit">Add</button>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        ></input>
+        <button type="submit" onClick={this.handleItems}>
+          Add
+        </button>
         <ul>{try3}</ul>
       </div>
     );
